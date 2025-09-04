c8ce7738c8312a5f3e0ed3039f9d31d2f5149c3b
myname993056 <Aos404@noreply.gitcode.com>
2025-09-04 17:21:50 +0800

    更新05

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -157,7 +157,7 @@ const Features = () => (
       <div className="grid h-[135vh] w-full grid-cols-2 grid-rows-3 gap-7">
         <BentoTilt className="bento-tilt_1 row-span-1 md:col-span-1 md:row-span-2">
           <BentoCard
-            src="videos/feature-2.mp4"
+            src="videos/5.mp4"
             title={
               <>
                 小狗-德克士
